df8a04a609fdb54afbe54e64c1d0e0b0335eb5ef
vitorsaldivar <vitor.saldivar@gmail.com>
2022-02-15 19:35:44 -0300

    fim do dia

---
+++ ex08.js
@@ -0,0 +1,11 @@
+var vel = 48
+while (vel < 62) {
+    if(vel > 60) {
+        console.log('Vc ultrapassou a velocidade limite. MULTADO!')
+        vel++
+    }
+    else {
+        console.log('Dirija sempre usando cinto de segurança!')
+        vel++
+    }
+}
